6a004586cb27f69a7076b338d40bf7c75ea1fb38
Rwagaso <stevenrwa31@gmail.com>
2023-08-24 03:05:49 -0700

    more

--- main.c
+++ main.c
@@ -1,4 +1,5 @@
 #include "monty.h"
+ssize_t getliner(char **lineptr,size_t*n,FILE*stream);
 bus_t bus = {NULL, NULL, NULL, 0};
 /**
 * main - monty code interpreter
@@ -30,7 +31,7 @@ int main(int argc, char *argv[])
 	while (read_line > 0)
 	{
 		content = NULL;
-		read_line = getline(&content, &size, file);
+		read_line = getliner(&content, &size, file);
 		bus.content = content;
 		counter++;
 		if (read_line > 0)
